f538e84935db20839d476cbbfd0f1a32c96dd460
jensuid <aji.daryanto@gmail.com>
2025-03-11 16:24:42 +0700

    astro config  github deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site:'https://jensuid/github.io',
-  base:'astro-play'
+  base:'astro-play',
   integrations: [
     react(),
     tailwindcss({
